50bff2117b7dde16ea6ca30cc6bf6fd96e9162e5
yogaaltariz <yoga.altariz@gmail.com>
2020-09-09 12:53:30 +0700

    banner responsive

--- klikmedic.js
+++ klikmedic.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36481" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35103" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
